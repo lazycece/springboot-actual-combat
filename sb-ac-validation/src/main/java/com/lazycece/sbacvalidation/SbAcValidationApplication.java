69af8b6388fef9b17b86a30b638f0f747d01aa91
lazycece <lazycece@gmail.com>
2018-11-20 15:36:35 +0800

    innitial validation project

---
+++ sb-ac-validation/src/main/java/com/lazycece/sbacvalidation/SbAcValidationApplication.java
@@ -0,0 +1,12 @@
+package com.lazycece.sbacvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAcValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAcValidationApplication.class, args);
+	}
+}

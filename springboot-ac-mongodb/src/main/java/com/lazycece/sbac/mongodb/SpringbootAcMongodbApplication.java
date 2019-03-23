d77156b64ef2659dc481be4ba4500aa98fcdb152
lazycece <lazycece@gmail.com>
2019-03-23 15:49:10 +0800

    modify

---
+++ springboot-ac-mongodb/src/main/java/com/lazycece/sbac/mongodb/SpringbootAcMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.sbac.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAcMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAcMongodbApplication.class, args);
+    }
+
+}
---
+++ springboot-ac-redis/src/main/java/com/lazycece/sbac/redis/SpringbootAcRedisApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.sbac.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAcRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAcRedisApplication.class, args);
+    }
+
+}

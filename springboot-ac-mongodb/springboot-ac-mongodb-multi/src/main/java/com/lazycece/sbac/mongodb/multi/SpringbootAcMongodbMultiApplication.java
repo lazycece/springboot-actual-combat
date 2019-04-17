29eccecc2d4dc4b5109fb3dd1c145ecb30179390
lazycece <lazycece@gmail.com>
2019-04-17 23:21:29 +0800

    modify

---
+++ springboot-ac-mongodb/springboot-ac-mongodb-multi/src/main/java/com/lazycece/sbac/mongodb/multi/SpringbootAcMongodbMultiApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.sbac.mongodb.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAcMongodbMultiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAcMongodbMultiApplication.class, args);
+    }
+
+}
---
+++ springboot-ac-mongodb/springboot-ac-mongodb-simple/src/main/java/com/lazycece/sbac/mongodb/simple/SpringbootAcMongodbSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.sbac.mongodb.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAcMongodbSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAcMongodbSimpleApplication.class, args);
+    }
+
+}

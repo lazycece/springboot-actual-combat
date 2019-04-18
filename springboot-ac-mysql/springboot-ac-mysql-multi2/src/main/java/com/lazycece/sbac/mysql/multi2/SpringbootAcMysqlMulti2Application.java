a02a720f090cd25eee0963d4e532d46970fcbec0
lazycece <lazycece@gmail.com>
2019-04-18 15:48:03 +0800

    modify

---
+++ springboot-ac-mysql/springboot-ac-mysql-multi2/src/main/java/com/lazycece/sbac/mysql/multi2/SpringbootAcMysqlMulti2Application.java
@@ -0,0 +1,13 @@
+package com.lazycece.sbac.mysql.multi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAcMysqlMulti2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAcMysqlMulti2Application.class, args);
+	}
+
+}
---
+++ springboot-ac-mysql/springboot-ac-mysql-multi2/src/test/java/com/lazycece/sbac/mysql/multi2/SpringbootAcMysqlMulti2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lazycece.sbac.mysql.multi2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootAcMysqlMulti2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

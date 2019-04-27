86e6d8c8b6efdfa01c4d8d77fabe9301bff8f2d4
lazycece <lazycece@gmail.com>
2019-04-27 19:45:59 +0800

    modify

--- springboot-ac-mysql/springboot-ac-mysql-multi2/src/main/java/com/lazycece/sbac/mysql/multi2/SpringbootAcMysqlMulti2Application.java
+++ springboot-ac-mysql/springboot-ac-mysql-multi2/src/main/java/com/lazycece/sbac/mysql/multi2/SpringbootAcMysqlMulti2Application.java
@@ -1,13 +1,11 @@
 package com.lazycece.sbac.mysql.multi2;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(
         exclude = {DataSourceAutoConfiguration.class})
-@MapperScan(basePackages = {"com.lazycece.sbac.mysql.data.dao"})
 public class SpringbootAcMysqlMulti2Application {
 
     public static void main(String[] args) {

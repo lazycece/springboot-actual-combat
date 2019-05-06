6268e18de70b242c8bb8befd76ca30d26bfa7e6e
lazycece <lazycece@gmail.com>
2019-05-06 21:59:48 +0800

    modify

--- springboot-ac-mongodb/springboot-ac-mongodb-multi/src/main/java/com/lazycece/sbac/mongodb/multi/config/MongoConfig.java
+++ springboot-ac-mongodb/springboot-ac-mongodb-multi/src/main/java/com/lazycece/sbac/mongodb/multi/config/MongoConfig.java
@@ -4,14 +4,15 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-
-import javax.annotation.Resource;
 
 /**
  * @author lazycece
@@ -19,13 +20,32 @@ import javax.annotation.Resource;
 @Configuration
 public class MongoConfig {
 
-    @Resource(name = "mongoDefaultProperties")
-    private MongoProperties mongoDefaultProperties;
 
-    @Resource(name = "mongoLogProperties")
-    private MongoProperties mongoLogProperties;
+    @Primary
+    @Bean(name = "mongoMasterProperties")
+    @ConfigurationProperties(prefix = "mongodb.master")
+    public MongoProperties mongoMasterProperties() {
+        return new MongoProperties();
+    }
 
-    private MongoDbFactory mongoDefaultFactory(MongoProperties mongoProperties) {
+    @Bean(name = "mongoLogProperties")
+    @ConfigurationProperties(prefix = "mongodb.log")
+    public MongoProperties mongoLogProperties() {
+        return new MongoProperties();
+    }
+
+    @Primary
+    @Bean("mongoTemplate")
+    public MongoTemplate mongoMasterTemplate(@Qualifier("mongoMasterProperties") MongoProperties mongoProperties) {
+        return new MongoTemplate(mongoDbFactory(mongoProperties));
+    }
+
+    @Bean("mongoLogTemplate")
+    public MongoTemplate mongoLogTemplate(@Qualifier("mongoLogProperties") MongoProperties mongoProperties) {
+        return new MongoTemplate(mongoDbFactory(mongoProperties));
+    }
+
+    private MongoDbFactory mongoDbFactory(MongoProperties mongoProperties) {
         ServerAddress serverAddress = new ServerAddress(mongoProperties.getHost(), mongoProperties.getPort());
         MongoClient mongoClient = new MongoClient(
                 serverAddress,
@@ -33,15 +53,5 @@ public class MongoConfig {
                         mongoProperties.getDatabase(), mongoProperties.getPassword()),
                 new MongoClientOptions.Builder().build());
         return new SimpleMongoDbFactory(mongoClient, mongoProperties.getDatabase());
-    }
-
-    @Bean("mongoTemplate")
-    public MongoTemplate mongoDefaultTemplate() {
-        return new MongoTemplate(mongoDefaultFactory(mongoDefaultProperties));
-    }
-
-    @Bean("mongoLogTemplate")
-    public MongoTemplate mongoLogTemplate() {
-        return new MongoTemplate(mongoDefaultFactory(mongoLogProperties));
     }
 }

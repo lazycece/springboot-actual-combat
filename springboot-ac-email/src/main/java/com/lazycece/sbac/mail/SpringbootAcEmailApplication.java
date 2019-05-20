bc910ff75747e961c8e37063e985bd8dfe87341b
lazycece <lazycece@gmail.com>
2019-05-20 13:46:25 +0800

    init sbac-email

---
+++ springboot-ac-email/src/main/java/com/lazycece/sbac/mail/SpringbootAcEmailApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.sbac.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAcEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAcEmailApplication.class, args);
+	}
+
+}

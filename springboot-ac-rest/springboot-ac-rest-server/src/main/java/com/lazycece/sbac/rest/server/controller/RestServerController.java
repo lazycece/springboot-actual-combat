405c54ae4b7c5ee83b3182fcfd671d9dbd8824ed
lazycece <lazycece@gmail.com>
2019-05-22 19:56:29 +0800

    modify

--- springboot-ac-rest/springboot-ac-rest-server/src/main/java/com/lazycece/sbac/rest/server/controller/RestServerController.java
+++ springboot-ac-rest/springboot-ac-rest-server/src/main/java/com/lazycece/sbac/rest/server/controller/RestServerController.java
@@ -1,5 +1,6 @@
 package com.lazycece.sbac.rest.server.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/5/22
  */
 @RestController
+@Slf4j
 public class RestServerController {
 
     @GetMapping("/rest/{name}")
     public String rest(@PathVariable String name) {
+        log.info("request -> {}", name);
         return "Hello, " + name + " !";
     }
 

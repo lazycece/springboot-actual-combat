a5d060b9cde93b8273b3be1e60ea061abc1f487e
lazycece <lazycece@gmail.com>
2019-06-10 18:14:24 +0800

    modify

---
+++ springboot-ac-task/src/main/java/com/lazycece/sbac/task/SpringbootAcTaskApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.sbac.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAcTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAcTaskApplication.class, args);
+	}
+
+}
